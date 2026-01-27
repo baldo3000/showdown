48c426908430fa3a3549f47a0181236b71b3bf17
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2026-01-27 16:42:56 +0100

    feat: add basic screen configuration

---
+++ core/src/main/kotlin/me/baldo3000/showdown/Showdown.kt
@@ -0,0 +1,45 @@
+package me.baldo3000.showdown
+
+import com.badlogic.ashley.core.Engine
+import com.badlogic.ashley.core.PooledEngine
+import com.badlogic.gdx.Application
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.utils.viewport.FitViewport
+import ktx.app.KtxGame
+import ktx.log.logger
+import me.baldo3000.showdown.view.screens.GameScreen
+import me.baldo3000.showdown.view.screens.HomeScreen
+import me.baldo3000.showdown.view.screens.ShowdownScreen
+
+const val UNIT_SCALE = 1 / 16f
+
+/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
+class Showdown : KtxGame<ShowdownScreen>() {
+    val gameViewport = FitViewport(16f, 9f)
+    val batch: Batch by lazy { SpriteBatch() }
+    val engine: Engine by lazy {
+        PooledEngine(10, 1000, 10, 1000).apply {
+
+        }
+    }
+
+    override fun create() {
+        Gdx.app.logLevel = Application.LOG_DEBUG
+        log.debug { "Game instance created" }
+        addScreen(HomeScreen(this))
+        addScreen(GameScreen(this))
+        setScreen<GameScreen>()
+    }
+
+    override fun dispose() {
+        super.dispose()
+        log.debug { "Max amount of sprites: ${(batch as SpriteBatch).maxSpritesInBatch}" }
+        batch.dispose()
+    }
+
+    companion object {
+        private val log = logger<Showdown>()
+    }
+}
---
+++ core/src/main/kotlin/me/baldo3000/showdown/view/screens/HomeScreen.kt
@@ -0,0 +1,22 @@
+package me.baldo3000.showdown.view.screens
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
+import ktx.log.logger
+import me.baldo3000.showdown.Showdown
+
+class HomeScreen(game: Showdown) : ShowdownScreen(game) {
+    override fun show() {
+        log.debug { "HomeScreen is shown" }
+    }
+
+    override fun render(delta: Float) {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.A)) {
+            game.setScreen<GameScreen>()
+        }
+    }
+
+    companion object {
+        private val log = logger<HomeScreen>()
+    }
+}
