76a19f034caf1e4024ac2c5cd0e4b2b6afd3d8ca
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2026-01-27 18:23:09 +0100

    fix: include base screen class

---
+++ core/src/main/kotlin/me/baldo3000/showdown/view/screens/ShowdownScreen.kt
@@ -0,0 +1,18 @@
+package me.baldo3000.showdown.view.screens
+
+import com.badlogic.ashley.core.Engine
+import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.utils.viewport.Viewport
+import ktx.app.KtxScreen
+import me.baldo3000.showdown.Showdown
+
+abstract class ShowdownScreen(
+    val game: Showdown,
+    val batch: Batch = game.batch,
+    val gameViewport: Viewport = game.gameViewport,
+    val engine: Engine = game.engine
+) : KtxScreen {
+    override fun resize(width: Int, height: Int) {
+        gameViewport.update(width, height, true)
+    }
+}
