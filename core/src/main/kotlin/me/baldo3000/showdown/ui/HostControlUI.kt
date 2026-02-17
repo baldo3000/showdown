463b70dd4e23ff0b4e496dffc6eb4b37b863e2e6
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2026-02-17 22:42:12 +0100

    fix: spacing on host control button

--- core/src/main/kotlin/me/baldo3000/showdown/ui/HostControlUI.kt
+++ core/src/main/kotlin/me/baldo3000/showdown/ui/HostControlUI.kt
@@ -1,41 +1,26 @@
 package me.baldo3000.showdown.ui
 
-import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton
-import com.badlogic.gdx.utils.Align
 import ktx.actors.onClick
-import ktx.scene2d.*
+import ktx.scene2d.KTableWidget
+import ktx.scene2d.scene2d
+import ktx.scene2d.table
+import ktx.scene2d.textButton
 
 class HostControlUI(
     private val onStartGame: () -> Unit
 ) {
     val table: KTableWidget
-    private val hostingLabel: Label
     private val startButton: TextButton
 
     init {
         table = scene2d.table {
-            hostingLabel = label("") { cell ->
-                wrap = true
-                setAlignment(Align.center)
-                cell.apply {
-                    padTop(20f)
-                    padBottom(20f)
-                }
-            }
-            row()
-
             startButton = textButton("Start game")
-
             setFillParent(true)
             center()
             pack()
         }
 
         startButton.onClick { onStartGame() }
-    }
-
-    fun updateHostingInfo(info: String) {
-        hostingLabel.setText(info)
     }
 }
