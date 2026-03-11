3a95b15baa501553e7eacb23dccb267beebe7a4b
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2026-03-11 21:18:14 +0100

    feat: add core support for posting and deleting at server

--- core/src/main/kotlin/me/baldo3000/showdown/network/LobbyHttpClient.kt
+++ core/src/main/kotlin/me/baldo3000/showdown/network/LobbyHttpClient.kt
@@ -2,18 +2,26 @@ package me.baldo3000.showdown.network
 
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
 import kotlinx.coroutines.*
 import kotlinx.serialization.json.Json
 import ktx.log.logger
+import me.baldo3000.showdown.dto.AddressDTO
 import me.baldo3000.showdown.dto.LobbiesDTO
 import me.baldo3000.showdown.network.api.Address
 
 private const val GET_TIMEOUT_MS = 3000L
 
 class LobbyHttpClient {
-    private val client = HttpClient(CIO)
+    private val client = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json()
+        }
+    }
     private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
     private var job: Job? = null
 
@@ -42,6 +50,36 @@ class LobbyHttpClient {
             } catch (e: Exception) {
                 log.error { "GET lobbies error: ${e.message}" }
                 onTimeout()
+            }
+        }
+    }
+
+    fun updateLobby(
+        lobbyServer: Address,
+        hostAddress: Address,
+        delete: Boolean = false
+    ) {
+        job?.cancel()
+        job = scope.launch {
+            try {
+                withTimeout(GET_TIMEOUT_MS) {
+                    val url = "http://${lobbyServer.ip}:${lobbyServer.port}$LOBBY_ROUTE"
+                    if (!delete) {
+                        client.post(url) {
+                            contentType(ContentType.Application.Json)
+                            setBody(AddressDTO(hostAddress.ip, hostAddress.port))
+                        }
+                    } else {
+                        client.delete(url) {
+                            contentType(ContentType.Application.Json)
+                            setBody(AddressDTO(hostAddress.ip, hostAddress.port))
+                        }
+                    }
+                }
+            } catch (e: CancellationException) {
+                throw e
+            } catch (e: Exception) {
+                log.error { "POST lobby error: ${e.message}" }
             }
         }
     }
