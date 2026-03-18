e9843550f8b324b347729d83769b3ad9e5dc071f
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2026-03-18 17:51:43 +0100

    chore: add shadow jar creation for server

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -3,6 +3,7 @@ val appName: String by project
 plugins {
     id("application")
     alias(libs.plugins.kotlin.plugin.serialization)
+    id("com.gradleup.shadow") version "9.3.1"
 }
 
 tasks.withType<JavaCompile> {
@@ -49,4 +50,3 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
-
